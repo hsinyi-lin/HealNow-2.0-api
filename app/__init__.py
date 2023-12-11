a5577b96f895c1361de81da3a25be1e94a54455e
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-12-12 00:11:54 +0800

    init project & add models & add routes

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from config import Config
+from flask_mail import Mail
+from flask_jwt_extended import JWTManager
+
+from .models import db
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    # with app.app_context():
+    #     db.create_all()  # This creates tables based on your models
+
+    mail = Mail(app)
+    jwt = JWTManager(app)
+
+    from app.auth.views import auth_bp
+    from app.post.views import post_bp
+    from app.comment.views import comment_bp
+    from app.save.views import save_bp
+    from app.mood.views import mood_bp
+
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(post_bp, url_prefix='/posts')
+    app.register_blueprint(comment_bp, url_prefix='/comments')
+    app.register_blueprint(save_bp, url_prefix='/saves')
+    app.register_blueprint(mood_bp, url_prefix='/moods')
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,174 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import BigInteger, CHAR, Column, Date, DateTime, Float, ForeignKey, Integer, Numeric, String, Table, Text, text
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class Clarification(db.Model):
+    __tablename__ = 'clarification'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('clarification_id_seq'::regclass)"))
+    title = Column(String)
+    content = Column(Text)
+    url = Column(Text)
+    publish_date = Column(Date)
+
+
+class Med(db.Model):
+    __tablename__ = 'med'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('med_id_seq'::regclass)"))
+    permit_num = Column(String)
+    permit_type = Column(String)
+    med_tw_name = Column(String)
+    med_en_name = Column(String)
+    med_type = Column(String)
+    composition = Column(String)
+    indications = Column(String)
+    how_to_use = Column(String)
+    permit_date = Column(Date)
+    edit_date = Column(Date)
+    expiration_date = Column(Date)
+    manufacturer = Column(String)
+    manufacturer_country = Column(String)
+    applicant_name = Column(String)
+    applicant_address = Column(String)
+
+
+class News(db.Model):
+    __tablename__ = 'news'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('news_id_seq'::regclass)"))
+    title = Column(String)
+    content = Column(Text)
+    url = Column(Text)
+    publish_date = Column(Date)
+
+
+class Pharmacy(db.Model):
+    __tablename__ = 'pharmacy'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('pharmacy_id_seq1'::regclass)"))
+    gender = Column(String)
+    pharmacy_name = Column(String)
+    phone = Column(String)
+    leader_name = Column(String)
+    city = Column(String)
+    town = Column(String)
+    street = Column(String)
+    full_address = Column(String)
+    latitude = Column(Numeric)
+    longitude = Column(Numeric)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    email = Column(String, primary_key=True)
+    password = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    gender = Column(CHAR(1), nullable=False)
+    created_time = Column(DateTime, nullable=False, default=datetime.now())
+    updated_time = Column(DateTime, nullable=False, default=datetime.now())
+
+
+class Verification(db.Model):
+    __tablename__ = 'verification'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('verification_id_seq'::regclass)"))
+    email = Column(String, nullable=False)
+    code = Column(String, nullable=False)
+    created_time = Column(DateTime, nullable=False, default=datetime.now())
+
+
+class Mood(db.Model):
+    __tablename__ = 'mood'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('mood_id_seq'::regclass)"))
+    email = Column(ForeignKey('user.email'), nullable=False)
+    content = Column(String, nullable=False)
+    ai_reply = Column(String, nullable=False)
+    created_time = Column(DateTime, nullable=False, default=datetime.now())
+
+    user = relationship('User')
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('post_id_seq'::regclass)"))
+    email = Column(ForeignKey('user.email'), nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    created_time = Column(DateTime, nullable=False, default=datetime.now())
+    updated_time = Column(DateTime, nullable=False, default=datetime.now())
+
+    user = relationship('User')
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class SavedClarification(db.Model):
+    __tablename__ = 'saved_clarification'
+
+    id = Column(Integer, nullable=False, server_default=text("nextval('saved_post_id_seq'::regclass)"))
+    clarification_id = Column(ForeignKey('clarification.id'), primary_key=True, nullable=False)
+    email = Column(ForeignKey('user.email'), primary_key=True, nullable=False)
+
+    clarification = relationship('Clarification')
+    user = relationship('User')
+
+
+class SavedMed(db.Model):
+    __tablename__ = 'saved_med'
+
+    id = Column(Integer, nullable=False, server_default=text("nextval('saved_post_id_seq'::regclass)"))
+    med_id = Column(ForeignKey('med.id'), primary_key=True, nullable=False)
+    email = Column(ForeignKey('user.email'), primary_key=True, nullable=False)
+
+    user = relationship('User')
+    med = relationship('Med')
+
+
+class SavedNew(db.Model):
+    __tablename__ = 'saved_news'
+
+    id = Column(Integer, nullable=False, server_default=text("nextval('saved_post_id_seq'::regclass)"))
+    news_id = Column(ForeignKey('news.id'), primary_key=True, nullable=False)
+    email = Column(ForeignKey('user.email'), primary_key=True, nullable=False)
+
+    user = relationship('User')
+    news = relationship('News')
+
+
+class SavedPost(db.Model):
+    __tablename__ = 'saved_post'
+
+    id = Column(Integer, nullable=False, server_default=text("nextval('saved_post_id_seq'::regclass)"))
+    post_id = Column(ForeignKey('post.id'), primary_key=True, nullable=False)
+    email = Column(ForeignKey('user.email'), primary_key=True, nullable=False)
+
+    user = relationship('User')
+    post = relationship('Post')
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('newtable_id_seq'::regclass)"))
+    post_id = Column(ForeignKey('post.id'), nullable=False)
+    email = Column(ForeignKey('user.email'), nullable=False)
+    content = Column(String, nullable=False)
+    created_time = Column(DateTime, nullable=False, default=datetime.now())
+    updated_time = Column(DateTime, nullable=False, default=datetime.now())
+
+    user = relationship('User')
+    post = relationship('Post')
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
