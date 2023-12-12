55253f43eebab8d14130f35fe6aa86ac18de013d
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-12-12 15:39:12 +0800

    feat: add config

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from datetime import timedelta
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=60)
+
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
