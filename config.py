5948688052776c5c71414f267cb8fc4b49690edc
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-12-13 17:17:55 +0800

    update: os environ get

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.gmail.com'
